ff35cb25150038e6f05519614ec9e67a894fc529
Shane Keniley <keniley1@illinois.edu>
2021-01-11 15:36:37 -0600

    Adding temporary gas temperature elastic kernel

---
+++ include/kernels/GasTemperatureElastic.h
@@ -0,0 +1,45 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#pragma once
+
+#include "ADKernel.h"
+
+class GasTemperatureElastic : public ADKernel
+{
+public:
+  static InputParameters validParams();
+  GasTemperatureElastic(const InputParameters & parameters);
+
+protected:
+  virtual ADReal computeQpResidual();
+
+  Real _r_units;
+  std::string _reaction_coeff_name;
+  std::string _reaction_name;
+
+  const ADMaterialProperty<Real> & _diffem;
+  const ADMaterialProperty<Real> & _muem;
+  const ADMaterialProperty<Real> & _alpha;
+  const MaterialProperty<Real> & _massGas;
+
+  const ADVariableGradient & _grad_potential;
+  const ADVariableValue & _em;
+  const ADVariableValue & _mean_en;
+  const ADVariableValue & _target;
+  const ADVariableGradient & _grad_em;
+  Real _massem;
+
+  Real _mult_factor;
+};
